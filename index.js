a6e356f5417b8a9f10200dad9275c8c893944e28
Max-Rus1977 <maxsvet77@yandex.ru>
2021-10-17 09:35:15 +0300

    router

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import Post from "./Post.js";
+import router from "./router.js";
 
 const PORT = 5000;
 const BD_URL = 'mongodb+srv://user:user@cluster0.mzbhb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -8,17 +8,7 @@ const BD_URL = 'mongodb+srv://user:user@cluster0.mzbhb.mongodb.net/myFirstDataba
 const app = express();
 
 app.use(express.json());
-
-app.post('/', async (req, res) => {
-  try {
-    const { auther, title, content, picture } = req.body;
-    const post = await Post.create({ auther, title, content, picture });
-    res.json(post);
-  }
-  catch (e) {
-    res.status(500).json(e);
-  }
-})
+app.use('/api', router);
 
 async function startApp() {
   try {
